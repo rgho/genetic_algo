ecd9b4cf264d3940e83257167d49ff22f857c6f7
R Ghosh <rishi2@gmail.com>
2013-09-30 17:07:49 -0400

    progress on many fronts

--- gen.py
+++ gen.py
@@ -24,21 +24,59 @@ def mutationsOf(pGene):
 	#mutates
 	return pGene
 
-def theOffspringOf(pGene1,pGene2):
+def theOffspringOf(pGene1,pGene2,pNumChildren):
 	#MATES THE TWO GENES USING THE
+
+	offsring = []
+	pNumChildren = -1
+	#while len(offsring) <= pNumChildren:
+
+	#choose pivot
+
+	
 	return pGene
 
 def someRandomGenes(generationSize):
-	return randomString(len(theSolution())for x in range(generationSize)
+	return [randomString(len(theSolution())) for x in range(generationSize)]
 
+def mainLoop(pGenes):
+	## CLEAR VARS
 
+	##INIT
+	numchildren = 6
 
+	
+	if not pGenes == NULL:
+		##CONSTANTS
+		generationSize = 1000
+		## IF FIRST GEN THEN CREATE FIRST GENERATION
+		genes = someRandomGenes(generationSize)
+	else:
+		generationSize = pGenes.len 
 
+	##SCORE THE FIRST GENERATION
+	totalGenFitness = 0 
+	for x in range(generationSize):
+		if genes[x] == theSolution(): solutionFound()
+		geneFitness[x] = theFitnessOf(genes[x])
+		totalGenFitness += geneFitness[x]
+	avgGenFitness = totalGenFitness/generationSize
 
+	## DELETE / KILL GENES THAT DONT BEAT THE AVERAGE
+	for x in range(generationSize):
+		### WARNING THIS CODE WONT LOOP PROPERLY I THINK
+		if geneFitness[x] <= avgGenFitness: genes.pop(x)
 
+	## MATE THE ONES THAT SURVIVE 
+	genes = theOffspringOf[genes]
+	genes = mutationsOf[genes]
+	mainLoop(genes)
 
-print randomString(20)
-print someRandomGenes(20)
+	return NULL
+
+yay = ''.join(['a','b','c','d','e'])
+print #yay[0:5]
+#len(someRandomGenes(20))
 
 
 
